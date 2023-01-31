ea8d3cc258c542e891359be2dc948f6b9ecdb51a
Pacey2706 <pacey2706@gmail.com>
2023-01-31 23:07:37 +0000

    add html to scores area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-console.log('hello Pacey');
+let button = getElementsByTagName('button');
+let player = getElementById('player-area');
+let computer = getElementById('computer-area');
+let playerScore = getElementById('player-score');
+let computerScore = getElementById('computer-score');
+let resultMessage = getElementsByClassName('message');
