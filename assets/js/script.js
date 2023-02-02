31c8db69ee3cd2d6d5326fceac9296b277180800
Pacey2706 <pacey2706@gmail.com>
2023-02-02 22:21:38 +0000

    add start game function and test with console.log

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
-let buttons = document.getElementsByTagName('button');
+let buttons = document.querySelectorAll('button');
 let player = document.getElementById('player-area');
 let computer = document.getElementById('computer-area');
 let playerScore = document.getElementById('player-score');
 let computerScore = document.getElementById('computer-score');
 let resultMessage = document.getElementsByClassName('message');
+let choices = ['rock', 'paper', 'scissor']
 
 for (let button of buttons){
+    button.addEventListener('click', function(){
+        let playerChoice = this.getAttribute('data-type');
+        startGame(playerChoice);
+    })
+};
 
-}
+function startGame(playerChoice){
+    player.innerHTML = `<img src="assets/images/${choices[playerChoice]}.png">`;
+    console.log('hello');
+};
+
+function getComputerChoice(){
+
+};
